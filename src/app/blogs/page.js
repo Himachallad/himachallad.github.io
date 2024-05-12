08fd3643f7d0a31671bb95fcc29fd1e96f677d4c
Nikhil Pathania <npathani@adobe.com>
2024-05-13 04:27:38 +0530

    Blog link addition

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,6 +1,6 @@
 import Blogs from '@/components/pages/Blogs';
 export default function BlogsPage() {
     return <main className="flex flex-col p-24">
-        <Blogs />
+        <Blogs disableLink />
     </main>;
 }
